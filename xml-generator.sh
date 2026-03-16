8a98cc26f4fd821cb2732dd48c1953f14dea4b41
DrSatrn <ramerize96@gmail.com>
2026-03-16 17:08:08 +1000

    chore: create gitignore, remove blank files and create xml-generator

--- xml-generator.sh
+++ xml-generator.sh
@@ -1,2 +1,2 @@
 # This script accepts a list of URLs (links.txt) and generates an xml favourites import for major web browsers
-# Use the learning resources.md to select which links you would like access to
+# Use the learning resources.md to select which links you would like access to and edit down the `links.txt` file in this repo
