db46c107842a6eb61343968379b19091f0f77dbe
Sebastian Treanor <ramerize96@gmail.com>
2026-03-16 16:36:47 +1000

    Create xml-generator.sh

---
+++ xml-generator.sh
@@ -0,0 +1,2 @@
+# This script accepts a list of URLs (links.txt) and generates an xml favourites import for major web browsers
+# Use the learning resources.md to select which links you would like access to
